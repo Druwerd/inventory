dc9d9cca513e42238614df870b34001fbc2032a3
Dru Ibarra <aibarra@Dru-Ibarras-MacBook-Pro.local>
2010-09-08 18:01:53 -0700

    don't show hidden fields

--- app/helpers/servers_helper.rb
+++ app/helpers/servers_helper.rb
@@ -3,6 +3,7 @@ module ServersHelper
   def display_attributes(server)
     html=""
     server.attributes.sort.each do |key, value|
+      next if key =~ /^_/ # don't show hidden fields
       html << %[
           <p>
             <b>#{key}:</b>
