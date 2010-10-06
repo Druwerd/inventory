347a988fcaf10004ed4822fe11c42e36ade5f37e
Dru Ibarra <aibarra@Dru-Ibarras-MacBook-Pro.local>
2010-10-06 14:47:42 -0700

    added load_time to site model added rake task: sites:update_stats

--- app/models/site.rb
+++ app/models/site.rb
@@ -3,11 +3,25 @@ class Site
   
   key :name, String, :unique => true, :required => true
   key :page_rank, String
+  key :load_time, String
   
-  before_save :get_page_rank
+  before_save :get_page_rank, :get_page_load_time
   
   def get_page_rank()
     self.page_rank = PageRankr.ranks(self.name)[:google]
+  end
+  
+  def get_page_load_time()
+    url = self.name
+    url = "http://#{url}" if url !~ /^https?:\/\//
+    url = URI.parse(url)
+    start_time = Time.now
+    req = Net::HTTP.start(url.host, url.port){ |http|
+      http.get('/')
+    }
+    self.load_time = Time.now - start_time
+  rescue 
+    self.load_time = -1
   end
   
 end
