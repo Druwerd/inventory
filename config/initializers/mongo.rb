9c81b9d86b816e497fdb560e5c631f5644f8a9a9
Dru Ibarra <aibarra@Dru-Ibarras-MacBook-Pro.local>
2010-10-28 16:57:08 -0700

    changed mongo mapper connection call for version 0.8.6

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -6,6 +6,6 @@ MongoMapper.database = Rails.configuration.database_configuration[Rails.env]["da
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|
-     MongoMapper.connection.connect_to_master if forked
+     MongoMapper.connection.connect if forked
    end
 end
